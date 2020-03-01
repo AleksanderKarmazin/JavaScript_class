cd0cdff25229aa6c788906993cf938e7863862bd
Aleksander Karmazin (Home_NEW_PC) <kubartphil@gmail.com>
2020-03-01 23:19:28 +0300

    1 March JS Commit

---
+++ lesson_01/sub_lesson_01.js
@@ -0,0 +1,20 @@
+console.log('Hello, World!');
+console.log('I am the King'); // For Lannisters!
+/*
+ * The night is dark and
+ * full of terrors.
+ */
+console.log('I am the King');
+
+console.log(typeof 'I am the King');
+console.log(typeof 1);
+console.log(typeof true );
+console.log(typeof null);
+
+// Это построчный коментарий 
+
+/*
+Это многострочный комментарий 
+Это многострочный комментарий 
+Это многострочный комментарий 
+*/
