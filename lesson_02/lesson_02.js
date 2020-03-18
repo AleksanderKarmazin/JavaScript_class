95b6956d8633bcc2b2854ab25531aaa1dbbd997c
Aleksander Karmazin (Home_NEW_PC) <kubartphil@gmail.com>
2020-03-18 20:31:03 +0300

    Create lesson 2

---
+++ lesson_02/lesson_02.js
@@ -0,0 +1,83 @@
+console.log('Hello, World!');
+console.log('I am the King'); // For Lannisters!
+/*
+ * The night is dark and
+ * full of terrors.
+ */
+console.log('I am the King');
+
+console.log(typeof 'I am the King');
+console.log(typeof 1);
+console.log(typeof true );
+console.log(typeof null);
+
+var a = 1;
+
+if (a ===10 ) {
+    console.log('I am the King');
+}
+else  {
+    console.log('FUCK');
+};
+
+
+
+var a = 1;
+
+var  cl = (a ===10 ) ? 'yes' : 'no'
+    console.log(cl);
+
+
+
+
+var a = 1;
+
+var day = "1"
+var day = "11"
+var day = "112"
+
+switch (day) {
+    case "10":
+        {
+            console.log(10);
+        }
+        break;
+    case "11":
+        {
+            console.log(11);
+        }
+        break;
+    case "112":
+        {
+            console.log(112);
+        }
+        break;
+    
+
+};
+var day = "119"
+
+switch (day) {
+    case "10":
+        {
+            console.log(10);
+        }
+        break;
+    case "11":
+        {
+            console.log(11);
+        }
+        break;
+    case "112":
+        {
+            console.log(112);
+        }
+        break;
+    default:
+        {
+            console.log("no match");
+        };
+
+};
+
+
