e3e645763901088248a9ff59161e89f8987ca43f
Aleksander Karmazin (Home_NEW_PC) <kubartphil@gmail.com>
2020-05-17 02:03:16 +0300

    First_page_Express

--- Node_JS/First_class/L9_Express/express_app/routes/users.js
+++ Node_JS/First_class/L9_Express/express_app/routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('index_next');
+});
+
+
+
+
+/* GET users listing. 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+*/
 module.exports = router;
