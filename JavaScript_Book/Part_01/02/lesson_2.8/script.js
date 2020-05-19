b18c2398105c64d4b97365106422881305ed8b9b
Aleksander Karmazin (Home_NEW_PC) <kubartphil@gmail.com>
2020-05-19 19:42:01 +0300

    19 may commit

---
+++ JavaScript_Book/Part_01/02/lesson_2.8/script.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+alert ('This is lasson 01');
+var user_name = prompt ('What is your name');
+console.log (user_name);
+
+var is_adult = confirm('Are you adult?');
+console.log (is_adult);
+
+*/
