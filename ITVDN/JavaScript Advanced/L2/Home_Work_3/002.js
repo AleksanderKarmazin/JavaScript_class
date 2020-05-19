5c2200839a9c1d6faf83ba1f10f090c53c433a25
Aleksander Karmazin (Home_NEW_PC) <kubartphil@gmail.com>
2020-05-19 20:17:31 +0300

    19 may dark

--- ITVDN/JavaScript Advanced/L2/Home_Work_3/002.js
+++ ITVDN/JavaScript Advanced/L2/Home_Work_3/002.js
@@ -8,7 +8,7 @@ var array_2 = ['hello ', 7 ,'567','Миша', 5678968,'w']
 document.write("<hr/>"); 
 document.write(array_1); 
 
-document.write("<h1>Всегда используйте «use strict»</h1>"); 
+document.write("<h1>Всегда используйте «use strict» ДА </h1>"); 
 
 
 var array_3 = [ , , ]
