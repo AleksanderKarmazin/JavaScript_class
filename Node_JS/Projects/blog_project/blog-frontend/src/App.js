4679bb3dab7929af4a86bb88b0447e7881d0e168
Aleksander Karmazin (Home_NEW_PC) <kubartphil@gmail.com>
2020-06-02 21:14:53 +0300

    2 June final

--- Node_JS/Projects/blog_project/blog-frontend/src/App.js
+++ Node_JS/Projects/blog_project/blog-frontend/src/App.js
@@ -10,18 +10,48 @@ function App() {
       />
       <div className="container">
         <div className="content">
-          <button type="button" className="btn btn-primary">
+          <button type="submit" className="btn btn-primary">
             Add Post
           </button>
           <div className="content">
-            <div className="post-items">
+            {/*<div className="post-items">
               <PostItem _id="1" title = "Загловок статьи" createdAt = {'' + new Date()} />
-              <PostItem _id="1" title = "Загловок статьи" createdAt = {'' + new Date()} />
-              <PostItem _id="1" title = "Загловок статьи" createdAt = {'' + new Date()} />
-              <PostItem _id="1" title = "Загловок статьи" createdAt = {'' + new Date()} />
-              <PostItem _id="1" title = "Загловок статьи" createdAt = {'' + new Date()} />
-              <PostItem _id="1" title = "Загловок статьи" createdAt = {'' + new Date()} />
-            </div>
+            </div>*/}
+            <form className="add-form">
+              <div className="form-group">
+                <div className="add-form_row">
+                  <h4>
+                    <label for="title">Title</label>
+                  </h4>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="title"
+                    placeholder="name@example.com"
+                  />
+                </div>
+                <div className="add-form_row">
+                  <h4>
+                    <label for="image">Image URL</label>
+                  </h4>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="image"
+                    placeholder="Enter image URL"
+                  />
+                </div>
+                <div className="add-form_row">
+                  <h4>
+                    <label for="discription">Discription</label>
+                  </h4>
+                  <textarea rows="10" className="form-control" id="discription" placeholder="Enter discription" />
+                </div>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
           </div>
         </div>
       </div>
